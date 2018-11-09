7c6a16f1a863296f3ebcce9e649d4ec360e7fbf7
Emine Ikuta <nagatoyuki0922@gmail.com>
2018-11-09 08:00:07 +0000

    add_association

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+
+  get 'favorites/destroy'
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
+  resources :favorites, only: [:create, :destroy]
   resources :blogs do
     collection do
       post :confirm
