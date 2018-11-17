4cd416a5a49cc088d7a3dbac5fa432b9041bea66
Emine Ikuta <nagatoyuki0922@gmail.com>
2018-11-17 07:19:58 +0000

    add_contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-
-  get 'favorites/destroy'
-
+  resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :favorites, only: [:create, :destroy]
@@ -12,4 +9,5 @@ Rails.application.routes.draw do
     end
   end
   root 'top#index'
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
